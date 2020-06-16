cb4eea16f255ab5aff3ffa77203e0a230bbded43
Mike Finch <mikefinch87@gmail.com>
2020-06-15 23:01:37 -0400

    initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_iDFdI3q53', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'u1hrroffmfgm4av270g2htpqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
